f6c49bec9fc1ce08963ecf4eeeb802d7b2c3b4e2
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-13 02:24:03 +0100

    initialization of data.sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,55 @@
+INSERT INTO ALBUM (PERFORMER, TITLE, RELEASE_DATE)
+VALUES
+       ('Kimbra', 'Vows', '2011-08-29' ),
+       ('Kimbra', 'Vows', '2012-05-22' );
+
+INSERT INTO TRACK (TITLE, DURATION)
+VALUES
+('Settle Down',	'00:04:17'),
+('Cameo Lover',	'00:04:03'),
+('Two Way Street',	'00:04:29'),
+('Old Flame',	'00:04:27'),
+('Good Intent',	'00:03:32'),
+('Plain Gold Ring',	'00:04:03'),
+('Call Me',	'00:04:32'),
+('Limbo',	'00:03:52'),
+('Wandering Limbs',	'00:05:27'),
+('Withdraw',	'00:04:07'),
+('The Build Up',	'00:08:21'),
+
+('Something In The Way You Are', '00:04:23'),
+('Come Into My Head', '00:04:39'),
+('Sally I Can See You', '00:03:58'),
+('Posse', '00:05:07'),
+('Home', '00:03:02'),
+('Warrior', '00:04:13');
+
+INSERT INTO ALBUM_TRACKS (ALBUM_ID, TRACKS_ID)
+VALUES
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(1,5),
+(1,6),
+(1,7),
+(1,8),
+(1,9),
+(1,10),
+(1,11);
+
+INSERT INTO ALBUM_TRACKS (ALBUM_ID, TRACKS_ID)
+VALUES
+    (2,1),
+    (2,12),
+    (2,2),
+    (2,3),
+    (2,4),
+    (2,5),
+    (2,6),
+    (2,13),
+    (2,14),
+    (2,15),
+    (2,16),
+    (2,11),
+    (2,17);
